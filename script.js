fadfc7d4a87caeea335cb4c7465a06734fe35f97
KokeiCreativa <kokeicreativa@gmail.com>
2025-07-25 19:46:36 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function startChat() {
-    alert('Fitur chatbot akan segera tersedia!');
-}
-
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Pesan berhasil dikirim!');
-});
